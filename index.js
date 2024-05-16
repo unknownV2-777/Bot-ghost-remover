b841632e280b7eef4d3671dc43118b7da0be7161
unknownV2-777 <132095352+unknownV2-777@users.noreply.github.com>
2024-05-16 17:53:58 +0200

    index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new Client({
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('YaY Your Bot Status Changed✨');
+  res.send('Status');
 });
 app.listen(port, () => {
   console.log(`🔗 Listening to RTX: http://localhost:${port}`);
@@ -74,7 +74,7 @@ function updateStatusAndSendMessages() {
 
   client.user.setPresence({
     activities: [{ name: currentStatus, type: ActivityType.Custom}],
-    status: 'dnd',
+    status: 'Test',
   });
 
   
@@ -91,9 +91,9 @@ function updateStatusAndSendMessages() {
 }
 
 client.once('ready', () => {
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    Real ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    LOL`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    Skull`);
   updateStatusAndSendMessages();
 
   setInterval(() => {
